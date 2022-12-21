5fa992575e663852823601e12c1eba91aad7f665
Amarshankar <amarshankar.taurus@gmail.com>
2022-12-21 12:36:38 +0530

    icon added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:holidayinn/screens/guest%20details/guest_details.dart';
 import 'package:holidayinn/screens/scan/scan_page.dart';
 import 'package:holidayinn/screens/splash%20screen/splash_screen.dart';
@@ -12,22 +13,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title:'Aadhar Scanner',
-
+      title: 'Aadhar Scanner',
       initialRoute: '/',
       routes: {
-        '/':(context)=> SplashScreen1(),
-        'guest detail':(context)=>GuestDetails(),
-        //'denied':(context)=>Denied(res),
-        'sucess':(context)=>SucessScreen(),
-        'scan':(context)=>ScanPage(),
+        '/': (context) => SplashScreen1(),
+        'guest detail': (context) => GuestDetails(),
+        //'denied':(context)=>Denied(),
+        'sucess': (context) => SucessScreen(),
+        'scan': (context) => ScanPage(),
       },
     );
   }
 }
-
